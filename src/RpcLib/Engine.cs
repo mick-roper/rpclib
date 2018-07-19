31f0c6ffd3abe1e46580581b7948f8857a241636
Mick Roper <michael.roper@drax.com>
2018-07-19 14:20:02 +0100

    added packages to test project

---
+++ src/RpcLib/Engine.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpcLib
+{
+    public sealed class Engine
+    {
+    }
+}
---
+++ tests/RpcLib.Tests/EngineTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RpcLib.Tests
+{
+    public class EngineTests
+    {
+    }
+}
